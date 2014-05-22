d32904686f74dc2597dbd0afbfd16598e36d1446
Ilya S <public.ilya.s@gmail.com>
2014-05-22 22:18:07 +0700

    pass the test

--- lib/transformer.rb
+++ lib/transformer.rb
@@ -1,7 +1,11 @@
 class Transformer
 
     def transform command
-        command
+        command.gsub /\s\-([\-a-z]+)\s([^\s]+)/i do |match|
+            key, value = match.split " "
+            " #{key} #{value}" if key.start_with? "-" 
+            " #{key}=#{value}"
+        end
     end
 
 end
