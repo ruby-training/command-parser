22b3d15b2493371f3bead0212dafbdc1b0637921
Ilya S <public.ilya.s@gmail.com>
2014-05-23 19:30:02 +0700

    add tests for Parser#new

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,9 @@
 class Parser
 
+    def initialize transformer
+        raise ArgumentError, "Got #{transformer.class}, not Transformer" \
+                                        unless transformer.instance_of? Transformer 
+        @transformer = transformer
+    end
+
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,12 +3,18 @@ require_relative '../lib/parser'
 describe Parser do
 
     before :each do
-        @parser = Parser.new
+        @parser = Parser.new(Transformer.new)
     end
     
     describe "#new" do
         it "is instantiable" do
             @parser.should be_an_instance_of Parser
+        end
+
+        it "validates the first argument" do
+            expect {
+                Parser.new nil
+            }.to raise_error ArgumentError, "Got NilClass, not Transformer"
         end
     end
 
