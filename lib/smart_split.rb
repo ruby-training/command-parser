c7fd17962ace5b6acc276988ae703a4a6a7ab6a4
Ilya S <public.ilya.s@gmail.com>
2014-05-23 20:19:00 +0700

    add be_aware_of and get_selectors to SmartSplit + specs

--- lib/smart_split.rb
+++ lib/smart_split.rb
@@ -1,3 +1,15 @@
 class SmartSplit
 
+    def initialize
+        @selectors = []
+    end
+
+    def be_aware_of selector
+        @selectors.push selector
+    end
+
+    def get_selectors
+        @selectors
+    end
+
 end
--- spec/smart_split_spec.rb
+++ spec/smart_split_spec.rb
@@ -12,4 +12,13 @@ describe SmartSplit do
         end
     end
 
+    describe "#be_aware_of" do
+        it "receives a selector" do
+            @smart_split.be_aware_of "["
+            @smart_split.be_aware_of "]"
+
+            @smart_split.get_selectors.should == ["[", "]"]
+        end
+    end
+
 end
